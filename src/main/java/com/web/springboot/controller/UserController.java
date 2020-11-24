e195b13088c8316138b94510980453ccd8c9b991
ZaborskiyAIG <zaborskiyaig@gmail.ru>
2020-11-24 18:25:12 +0700

    .

--- src/main/java/com/web/springboot/controller/UserController.java
+++ src/main/java/com/web/springboot/controller/UserController.java
@@ -13,15 +13,6 @@ import java.util.Set;
 @RequestMapping
 public class UserController {
 
-//	private UserService userService;
-//
-//	private RoleService roleService;
-//
-//	public UserController(UserService userService, RoleService roleService) {
-//		this.userService = userService;
-//		this.roleService = roleService;
-//	}
-
 	@GetMapping("/login")
 	public String loginPage() {
 		return "login";
@@ -29,8 +20,6 @@ public class UserController {
 
 	@GetMapping("/admin")
 	public String getUsers(Model model){
-	//	model.addAttribute("users", userService.listUsers());
-	//	model.addAttribute("roles", roleService.getRoles());
 		return "admin_page";
 	}
 
@@ -43,19 +32,11 @@ public class UserController {
 
 	@PostMapping(value = "/admin/add")
 	public String addUser(@ModelAttribute User user, @RequestParam(value = "role") Long[] rolesId){
-	//	userService.add(user, rolesId);
 		return "redirect:/admin";
 	}
 
-//	@PostMapping(value = "/admin/delete")
-//	public String deleteUser(@ModelAttribute("id") Long id){
-//	//	userService.remove(id);
-//		return "redirect:/admin";
-//	}
-
 	@PostMapping(value = "/admin/update")
 	public String updateUser(@ModelAttribute("user") User user, @RequestParam(value = "role", required = false) Long[] rolesId){
-		//userService.update(user, rolesId);
 		return "redirect:/admin";
 	}
 }
